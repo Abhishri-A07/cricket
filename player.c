ca5b594f150114864f8043cc38fd211f8f06038f
Sunmathi25 <2503717610422119@cit.edu.in>
2026-08-09 22:29:11 +0530

    Refactor player stats to use previous runs and wickets
    Updated player structure to use previous runs and wickets fields instead of current ones.

--- player.c
+++ player.c
@@ -5,13 +5,6 @@
 
 struct Player *head = NULL;
 struct Player *tail = NULL;
-float calculatePerformance(struct Player *p)
-{
-    return (p->runs * 0.5) +
-           (p->wickets * 20) +
-           (p->strikeRate * 0.2) +
-           (p->economy * 0.1);
-}
 void addPlayer()
 {
     struct Player *newPlayer;
@@ -39,9 +32,9 @@ void addPlayer()
     printf("Enter Player Role (Batsman/Bowler/All-Rounder): ");
     scanf(" %[^\n]", newPlayer->role);
     printf("Enter Previous Runs: ");
-    scanf("%d", &newPlayer->runs);
+    scanf("%d", &newPlayer->pruns);
     printf("Enter Previous Wickets: ");
-    scanf("%d", &newPlayer->wickets);
+    scanf("%d", &newPlayer->pwickets);
     printf("Enter Strike Rate: ");
     scanf("%f", &newPlayer->strikeRate);
     printf("Enter Economy: ");
@@ -79,8 +72,8 @@ void displayPlayer(struct Player *p)
     printf("Player ID       : %d\n", p->id);
     printf("Name            : %s\n", p->name);
     printf("Role            : %s\n", p->role);
-    printf("Previous Runs   : %d\n", p->runs);
-    printf("Wickets         : %d\n", p->wickets);
+    printf("Previous Runs   : %d\n", p->pruns);
+    printf("Wickets         : %d\n", p->pwickets);
     printf("Strike Rate     : %.2f\n", p->strikeRate);
     printf("Economy         : %.2f\n", p->economy);
     printf("Performance     : %.2f\n", p->performance);
@@ -118,9 +111,9 @@ void updatePlayer()
     printf("Enter New Role: ");
     scanf(" %[^\n]", p->role);
     printf("Enter New Previous Runs: ");
-    scanf("%d", &p->runs);
+    scanf("%d", &p->pruns);
     printf("Enter New Wickets: ");
-    scanf("%d", &p->wickets);
+    scanf("%d", &p->pwickets);
     printf("Enter New Strike Rate: ");
     scanf("%f", &p->strikeRate);
     printf("Enter New Economy: ");
@@ -172,8 +165,8 @@ void displayAllPlayers()
         printf("\nID          : %d", temp->id);
         printf("\nName        : %s", temp->name);
         printf("\nRole        : %s", temp->role);
-        printf("\nRuns        : %d", temp->runs);
-        printf("\nWickets     : %d", temp->wickets);
+        printf("\nRuns        : %d", temp->pruns);
+        printf("\nWickets     : %d", temp->pwickets);
         printf("\nStrike Rate : %.2f", temp->strikeRate);
         printf("\nEconomy     : %.2f", temp->economy);
         printf("\nPerformance : %.2f", temp->performance);
