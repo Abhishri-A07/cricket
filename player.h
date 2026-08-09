b47eb05bfc9580f1679a154c0563a4b05f514d20
Aarthx-07 <2503717610422065@cit.edu.in>
2026-08-09 22:07:52 +0530

    Created player.h

---
+++ player.h
@@ -0,0 +1,40 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+struct Player
+{
+    int id;
+    char name[30];
+    char role[20];
+
+    int runs;
+    int wickets;
+    float strikeRate;
+    float economy;
+    float performance;
+
+    int ballsFaced;
+    int out;
+
+    int ballsBowled;
+    int runsConceded;
+
+    struct Player *prev;
+    struct Player *next;
+};
+
+extern struct Player *head;
+extern struct Player *tail;
+
+float calculatePerformance(struct Player *p);
+
+void addPlayer();
+struct Player *searchPlayer(int id);
+void displayPlayer(struct Player *p);
+void searchAndDisplay();
+void updatePlayer();
+void deletePlayer();
+void displayAllPlayers();
+void freeDatabase();
+
+#endif
