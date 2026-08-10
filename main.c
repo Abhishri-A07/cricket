21f5ec351be303079d19b2cbdcb8cd21f09d87c5
Aarthx-07 <2503717610422065@cit.edu.in>
2026-08-10 20:15:53 +0530

    Update1 main.c

--- main.c
+++ main.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "player.h"
 
-int main()
+
+void playerDatabaseMenu(void)
 {
     int choice;
-    int totalOvers;
 
     do
     {
-        printf("\n\n========== CRICKET MANAGEMENT SYSTEM ==========\n");
+        printf("\n\n========== CRICKET PLAYER DATABASE ==========\n");
+
         printf("1. Add Player\n");
         printf("2. Delete Player\n");
         printf("3. Search Player\n");
         printf("4. Update Player\n");
         printf("5. Display All Players\n");
-        printf("6. Start Match\n");
-        printf("7. Performance Calculation\n");
-        printf("8. Player Ranking\n");
-        printf("9. Best Players\n");
-        printf("10. Sort Players\n");
-        printf("11. Exit\n");
-        printf("===============================================\n");
+        printf("6. Back\n");
+
+        printf("=============================================\n");
 
         printf("Enter your choice: ");
         scanf("%d", &choice);
+
 
         switch (choice)
         {
@@ -48,47 +48,279 @@ int main()
                 break;
 
             case 6:
-                printf("\nEnter number of overs: ");
-                scanf("%d", &totalOvers);
+                break;
 
-                if (totalOvers <= 0)
+            default:
+                printf("\nInvalid choice!\n");
+        }
+
+    } while (choice != 6);
+}
+
+
+void runAuction(void)
+{
+    AuctionTeam teamA;
+    AuctionTeam teamB;
+
+    AuctionResult results[100];
+
+    Player *temp;
+
+    int count = 0;
+    int choice;
+
+
+    initializeTeam(&teamA,
+                   "Team A");
+
+    initializeTeam(&teamB,
+                   "Team B");
+
+
+    temp = head;
+
+
+    if (temp == NULL)
+    {
+        printf("\nNo players available for auction.\n");
+        return;
+    }
+
+
+    while (temp != NULL)
+    {
+        printf("\nAuction player: %s\n",
+               temp->name);
+
+        printf("1. Auction this player\n");
+        printf("2. Skip\n");
+        printf("Enter choice: ");
+
+        scanf("%d", &choice);
+
+
+        if (choice == 1)
+        {
+            auctionPlayer(temp,
+                          &teamA,
+                          &teamB,
+                          &results[count]);
+
+            count++;
+        }
+
+
+        temp = temp->next;
+    }
+
+
+    displayFinalReport(results,
+                       count,
+                       &teamA,
+                       &teamB);
+}
+
+
+void runSquadSelection(void)
+{
+    Player squad[MAX_SQUAD];
+    Player xi[11];
+
+    int n;
+
+
+    getSquad(squad,
+             &n);
+
+    getLineup(squad,
+              n,
+              xi);
+
+    battingOrder(xi);
+
+    selectCaptain(xi);
+
+    selectKeeper(xi);
+
+    bowlingPlan(xi);
+
+    displayLineup(xi);
+}
+
+
+void runMatch(void)
+{
+    MatchController match;
+
+    int innings;
+
+
+    srand((unsigned int)time(NULL));
+
+    initializeMatch(&match);
+
+
+    printf("\n========================================\n");
+    printf("             CRICKET MATCH\n");
+    printf("========================================\n");
+
+
+    /*
+     * Toss
+     */
+
+    toss(&match);
+
+
+    /*
+     * First innings
+     */
+
+    startInnings(&match,
+                 1);
+
+
+    /*
+     * Actual ball-by-ball match
+     */
+
+    play(head,
+         MAX_OVERS);
+
+
+    /*
+     * Store first innings score
+     *
+     * Since play() maintains the player records,
+     * the controller is used here mainly for
+     * match state management.
+     */
+
+    match.state = INNINGS_COMPLETED;
+
+
+    printf("\nFirst innings completed.\n");
+
+
+    /*
+     * Second innings
+     */
+
+    printf("\nStarting second innings...\n");
+
+    startSecondInnings(&match);
+
+    play(head,
+         MAX_OVERS);
+
+
+    match.state = MATCH_COMPLETED;
+
+
+    printf("\n========================================\n");
+    printf("             MATCH COMPLETED\n");
+    printf("========================================\n");
+}
+
+
+int main(void)
+{
+    int choice;
+
+
+    do
+    {
+        printf("\n\n============================================\n");
+        printf("          CRICKET MANAGEMENT SYSTEM\n");
+        printf("============================================\n");
+
+        printf("1. Player Database\n");
+        printf("2. Player Auction\n");
+        printf("3. Squad / Playing XI Selection\n");
+        printf("4. Start Cricket Match\n");
+        printf("5. Performance Ranking\n");
+        printf("6. Best Players\n");
+        printf("7. Sort Players\n");
+        printf("8. Display All Players\n");
+        printf("9. Exit\n");
+
+        printf("============================================\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+
+        switch (choice)
+        {
+            case 1:
+                playerDatabaseMenu();
+                break;
+
+
+            case 2:
+                runAuction();
+                break;
+
+
+            case 3:
+                runSquadSelection();
+                break;
+
+
+            case 4:
+                if (head == NULL)
                 {
-                    printf("Invalid number of overs!\n");
+                    printf("\nAdd players before starting a match.\n");
                 }
                 else
                 {
-                    play(head, totalOvers);
+                    runMatch();
                 }
                 break;
 
-            case 7:
-                performance(head);
-                printf("\nPerformance calculated successfully.\n");
-                break;
 
-            case 8:
+            case 5:
+                performance(head);
                 ranking(head);
                 break;
 
-            case 9:
+
+            case 6:
+                performance(head);
                 bestplayer(head);
                 break;
 
-            case 10:
+
+            case 7:
+                performance(head);
                 sortplayer(head);
-                printf("\nPlayers sorted successfully.\n");
+
+                printf("\nPlayers sorted according to performance.\n");
+
+                ranking(head);
                 break;
 
-            case 11:
-                freeDatabase();
-                printf("\nPlayer database closed.\n");
+
+            case 8:
+                displayAllPlayers();
                 break;
+
+
+            case 9:
+                freeDatabase();
+
+                printf("\nExiting Cricket Management System...\n");
+                break;
+
 
             default:
                 printf("\nInvalid choice! Try again.\n");
         }
 
-    } while (choice != 11);
+
+    } while (choice != 9);
+
 
     return 0;
 }
