85633f2455325188474917da56a2ac368eeed8ef
Aarthx-07 <2503717610422065@cit.edu.in>
2026-08-10 19:51:47 +0530

    Update player.h

--- player.h
+++ player.h
@@ -31,7 +31,7 @@ struct Player
 extern struct Player *head;
 extern struct Player *tail;
 
-
+/* Player Database */
 void addPlayer();
 struct Player *searchPlayer(int id);
 void displayPlayer(struct Player *p);
@@ -41,13 +41,45 @@ void deletePlayer();
 void displayAllPlayers();
 void freeDatabase();
 
-/* Performance and sorting functions */
+/* Performance and Ranking */
 void performance(struct Player *head);
 void ranking(struct Player *head);
 void bestplayer(struct Player *head);
 void sortplayer(struct Player *head);
 
-/* Match playing function */
+/* Match */
+void batting(struct Player *striker, int runs);
+void bowling(struct Player *bowler, int runs);
 void play(struct Player *head, int totalOvers);
+
+/* Auction */
+typedef struct
+{
+    char name[20];
+    float budget;
+    float spent;
+    int playerCount;
+} Team;
+
+typedef struct
+{
+    int playerID;
+    char playerName[30];
+    char teamName[20];
+    float soldPrice;
+} AuctionResult;
+
+#define INITIAL_BUDGET 100.0f
+
+void initializeTeam(Team *team, const char name[]);
+void displayAuctionPlayer(struct Player *player);
+void auctionPlayer(struct Player *player,
+                   Team *teamA,
+                   Team *teamB,
+                   AuctionResult *result);
+void displayFinalReport(AuctionResult results[],
+                        int count,
+                        Team *teamA,
+                        Team *teamB);
 
 #endif
